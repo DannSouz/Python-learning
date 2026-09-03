b4b28ac2ee67830caa08319ed028b9282d41ac29
Danna Souza <danna174souza@gmail.com>
2026-09-02 22:19:37 -0400

    Delete Scripts - Py directory

--- Scripts - Py/exercicio-02-data-nascimento.py
+++
@@ -1,7 +0,0 @@
-"Desafio 2 - Python"
-
-Dia=input('Qual o dia do seu nascimento?')
-Mês=input('Qual o mês do seu nascimento?')
-Ano=input('Qual o ano do seu nascimento?')
-print('Você nasceu no', Dia, 'de', Mês, 'de', Ano,'. Correto?')
-
--- Scripts - Py/exercicio-03-soma.py
+++
@@ -1,4 +0,0 @@
-'Desafio 3 - Python'
-numero1=int(input('Primeiro número'))
-numero2=int(input('Segundo número'))
-print(numero1+numero2)
