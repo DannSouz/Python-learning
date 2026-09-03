51148be3053cba232a87867bee08151eefc3378b
Danna Souza <danna174souza@gmail.com>
2026-09-02 22:17:47 -0400

    Add files via upload

---
+++ Scripts - Py/exercicio-02-data-nascimento.py
@@ -0,0 +1,7 @@
+"Desafio 2 - Python"
+
+Dia=input('Qual o dia do seu nascimento?')
+Mês=input('Qual o mês do seu nascimento?')
+Ano=input('Qual o ano do seu nascimento?')
+print('Você nasceu no', Dia, 'de', Mês, 'de', Ano,'. Correto?')
+
